1a1ef2fd3604edf3902f56489e20b521ea89a6c1
Donbeni <donblackimboyo@gmail.com>
2023-04-26 15:51:46 +0200

    Tests

--- Tests/ValidateurTest.php
+++ Tests/ValidateurTest.php
@@ -1,34 +1,33 @@
 <?php
-/**
- * Ce fichier contient la classe Validateur, qui fournit des fonctionnalités utiles.
- *
- * PHP Version 7
- *
- * @category Class_Validateur.
- * @package  MonPackage.
- * @author   donbeni imboyo <donblackimboyo@gmail.com>
- * @license  https://www.example.com/license/ M.
- * @link     https://pedag.u-picardie.fr/moodle/upjv/?redirect=0
- */
-/**
- * Class Doc Comment
- *
- * @category Class_Validateur.
- * @package  MonPackage.
- * @author   donbeni imboyo <donblackimboyo@gmail.com>
- * @license  https://www.example.com/license/ M.
- * @link     https://pedag.u-picardie.fr/moodle/upjv/?redirect=0
- */
-
+class Validateur
+{
+    /**
+     * Validitatuer nous permet de trouver le lien de conception
+     *
+     * @param string $input Chaîne d'entrée à valider.
+     *
+     * @return bool vrai si l'entrée est valide, faux sinon.
+     */
+    public function check($input)
+    {
+        //donbeni est puissant.
+        if (strlen($input) < 2) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
 final class ValidateurTest extends PHPUnit\Framework\TestCase
 {
     public function testCheck()
     {
-        $Validateur = new Validateur();
-        $this->assertTrue($Validateur->check('abc')); 
-        $this->assertFalse($Validateur->check('a')); // la chaîne 'a' n'a pas une longueur valide et doit renvoyer false
+        $validateur = new Validateur();
+        
+        // Test avec une chaîne valide
+        $this->assertTrue($validateur->check('abc')); 
+        
+        // Test avec une chaîne invalide
+        $this->assertFalse($validateur->check('a'));
     }
 }
-
-
-
